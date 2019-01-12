5ada841263f2a0753fa70ba8ae5931cdd83e2ace
liliana Quinley <lilicuellar99@gmail.com>
2019-01-12 01:10:13 -0700

    added headlines and notes js

---
+++ controllers/headlines.js
@@ -0,0 +1,9 @@
+var scrape = require ('../scripts/scrape');
+var makeDate = require ("../scripts/date");
+
+// bring in the headline and note mongoose models
+var article = require ("../models/article");
+
+module.exports = {
+    
+}
